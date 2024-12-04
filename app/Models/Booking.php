0bab66226eb69b7f44f80e0d0fe5ddd60c4cb6a1
Kim Rune Møller <kimrune@hotmail.no>
2024-12-04 22:19:09 +0100

    adds Room relation

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
-    //
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
