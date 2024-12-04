98019d88a1b39825cc1c077424de6c8c2ba53aed
Kim Rune Møller <kimrune@hotmail.no>
2024-12-04 14:47:07 +0100

    adds bookings interface

--- resources/js/Pages/Display/Location/Location.tsx
+++ resources/js/Pages/Display/Location/Location.tsx
@@ -5,6 +5,13 @@ interface ILocation {
         id: string;
         name: string;
         displayMessage: string;
+        bookings: [
+            {
+                name: string;
+                start: string;
+                end: string;
+            },
+        ];
     };
 }
 
@@ -13,6 +20,7 @@ interface ILocationProps {
 }
 
 export default function Location({ location }: ILocationProps) {
+    console.log(location);
     return (
         <LocationLayout>
             <h1>{location.data.name} will display here</h1>
