17c322d2b04f92d5ffb29e101bc9e8f685cedcdb
Kim Rune Møller <kimrune@hotmail.no>
2024-12-04 14:46:51 +0100

    adds relations

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Location extends Model
 {
-    //
+    public function rooms(): HasMany
+    {
+        return $this->hasMany(Room::class);
+    }
+
+    public function bookings(): HasManyThrough
+    {
+        return $this->hasManyThrough(Booking::class, Room::class);
+    }
 }
