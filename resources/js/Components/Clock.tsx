185b11731c49ab0dec88ef758912876236400315
Kim Rune Møller <kimrune@hotmail.no>
2024-12-04 22:19:22 +0100

    Clock component

---
+++ resources/js/Components/Clock.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function Clock(props: props) {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <div className={props.className}>
+            {time.toLocaleTimeString(undefined, {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: false,
+                second: undefined,
+            })}
+        </div>
+    );
+}
+
+type props = {
+    className: string;
+};
