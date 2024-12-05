a01756ebe50d93b1e1c18f96f44d2f472593b4c5
Kim Rune Møller <kimrune@hotmail.no>
2024-12-05 14:26:11 +0100

    fix failing test

--- e2e/welcome.spec.ts
+++ e2e/welcome.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test";
 test("has title", async ({ page }) => {
     await page.goto("http://roomdisplay.test");
 
-    await expect(page).toHaveTitle("Welcome - Room Display");
+    await expect(page).toHaveTitle("Welcome - Laravel");
 });
