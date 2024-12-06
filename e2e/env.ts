ab479eee44c1788d063234e45d1d483b02a3dc8e
Kim Rune Møller <kimrune@hotmail.no>
2024-12-06 19:22:55 +0100

    adds venue name env var

--- e2e/env.ts
+++ e2e/env.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const APP_URL = process.env.APP_URL || "http://localhost";
 const APP_PORT = process.env.APP_PORT || "8000";
 const baseUrl = `${APP_URL}:${APP_PORT}`;
-const appName = process.env.VITE_VENUE_NAME || "Roomdisplay";
+const appName = process.env.VITE_APP_NAME || "Roomdisplay";
+const venueName = process.env.VITE_VENUE_NAME || "Roomdisplay";
 
-export { appName, baseUrl };
+export { appName, baseUrl, venueName };
