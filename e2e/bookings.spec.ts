3b72137244546e44a5014f51436375d894babc17
Kim Rune Møller <kimrune@hotmail.no>
2024-12-06 20:16:29 +0100

    improve bookings page test

--- e2e/bookings.spec.ts
+++ e2e/bookings.spec.ts
@@ -1,12 +1,26 @@
 import { expect, test } from "@playwright/test";
 import { baseUrl, venueName } from "./env";
 
-test("show bookings page", async ({ page }) => {
-    const locationSlug = "test-location";
-    const locationName = "Test Location";
-    await page.goto(`${baseUrl}/locations/${locationSlug}`);
+const locationSlug = "test-location";
+const locationName = "Test Location";
 
-    const heading = page.locator("h1");
+test.describe("Bookings", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto(`${baseUrl}/locations/${locationSlug}`);
+    });
 
-    await expect(heading).toHaveText(`${venueName} - ${locationName}`);
+    test("displays bookings page", async ({ page }) => {
+        const heading = page.locator("h1");
+
+        await expect(heading).toHaveText(`${venueName} - ${locationName}`);
+    });
+
+    test("displays bookings", async ({ page }) => {
+        const items = page.locator("li");
+        const bookings = page.locator("#bookings", {
+            has: items,
+        });
+
+        await expect(bookings).toBeVisible();
+    });
 });
