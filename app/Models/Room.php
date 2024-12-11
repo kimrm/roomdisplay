95dd378d24243a29e4bec5e2ce6de969a58ae135
Kim Rune Møller <kimrune@hotmail.no>
2024-12-11 13:22:41 +0100

    Adds function to generate unique slug from name

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Room extends Model
 {
     use HasFactory;
-    //
+
+    public static function generateUniqueSlug($name, $locationId)
+    {
+        $slug = Str::slug($name);
+        $originalSlug = $slug;
+        $count = 1;
+
+        while (self::where('slug', $slug)->where('location_id', $locationId)->exists()) {
+            $slug = "{$originalSlug}-{$count}";
+            $count++;
+        }
+
+        return $slug;
+    }
 }
