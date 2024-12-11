d056db3928563a1b4f48832fe3691054096b9777
Kim Rune Møller <kimrune@hotmail.no>
2024-12-11 15:23:21 +0100

    formatting

--- resources/js/Layouts/LocationLayout.tsx
+++ resources/js/Layouts/LocationLayout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren } from "react";
 
 export default function LocationLayout({ children }: PropsWithChildren) {
     return <div>{children}</div>;
