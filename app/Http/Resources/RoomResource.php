56913bd5296d70bc851f701c636acd6e4d0a172f
Kim Rune Møller <kimrune@hotmail.no>
2024-12-22 01:12:53 +0100

    Room resource

---
+++ app/Http/Resources/RoomResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoomResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $data = parent::toArray($request);
+        return collect($data)->mapWithKeys(function ($value, $key) {
+            $camelCaseKey = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key))));
+            return [$camelCaseKey => $value];
+        })->toArray();
+    }
+}
