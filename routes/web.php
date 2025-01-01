34162dd601b29107e9398d51d172596c2ce5b9df
Kim Rune Møller <kimrune@hotmail.no>
2025-01-01 18:29:36 +0100

    adds route for bookings admin

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\View\LocationController;
+use App\Http\Controllers\Admin\BookingController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,6 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('bookings', BookingController::class)->only(['index', 'store', 'destroy']);
 });
 
 require __DIR__ . '/dashboard.php';
