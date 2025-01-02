bac71901246320b5769be9ed9f8d745fb8b64de4
Kim Rune Møller <kimrune@hotmail.no>
2025-01-02 12:41:34 +0100

    rooms index page

---
+++ resources/js/Pages/Admin/Rooms/Index.tsx
@@ -0,0 +1,46 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { Room } from "@/types";
+
+interface RoomProps {
+    rooms: Room[];
+}
+
+export default function RoomsPage({ rooms }: RoomProps) {
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    Rom
+                </h2>
+            }
+        >
+            <Head title="Profile" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
+                        <p className="dark:text-white">Rom</p>
+                        <table>
+                            <caption className="sr-only">Rom</caption>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {rooms.map((room) => (
+                                    <tr key={room.id}>
+                                        <td>{room.id}</td>
+                                        <td>{room.name}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
