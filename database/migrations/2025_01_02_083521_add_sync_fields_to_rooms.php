82928befcbf9dc8db685d38af0b55f383ba97854
Kim Rune Møller <kimrune@hotmail.no>
2025-01-02 12:40:45 +0100

    sync fields migration

---
+++ database/migrations/2025_01_02_083521_add_sync_fields_to_rooms.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->string('service')->nullable()->after('display_message');
+            $table->string('calendar_id')->nullable()->after('display_message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->dropColumn('service');
+            $table->dropColumn('calendar_id');
+        });
+    }
+};
