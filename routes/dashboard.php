aae5605d739e838a061dc9cec7f2b8889f0b94b5
Kim Rune Møller <kimrune@hotmail.no>
2025-01-02 12:42:28 +0100

    routes reorganize

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,7 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Admin\BookingController;
+use App\Http\Controllers\Admin\RoomController;
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::prefix('/dashboard')->group(function () {
+        Route::get('/', function () {
+            return Inertia::render('Dashboard');
+        })->name('dashboard');
+
+        Route::prefix('/rooms')->group(function () {
+            Route::get('/', [RoomController::class, 'index'])->name('rooms.index');
+            Route::get('/create', [RoomController::class, 'create'])->name('rooms.create');
+            Route::post('/', [RoomController::class, 'store'])->name('rooms.store');
+            Route::get('/{room}/edit', [RoomController::class, 'edit'])->name('rooms.edit');
+            Route::patch('/{room}', [RoomController::class, 'update'])->name('rooms.update');
+            Route::delete('/{room}', [RoomController::class, 'destroy'])->name('rooms.destroy');
+        });
+
+        Route::prefix('/bookings')->group(function () {
+            Route::get('/', [BookingController::class, 'index'])->name('bookings.index');
+            Route::get('/create', [BookingController::class, 'create'])->name('bookings.create');
+            Route::post('/', [BookingController::class, 'store'])->name('bookings.store');
+            Route::get('/{booking}/edit', [BookingController::class, 'edit'])->name('bookings.edit');
+            Route::patch('/{booking}', [BookingController::class, 'update'])->name('bookings.update');
+            Route::delete('/{booking}', [BookingController::class, 'destroy'])->name('bookings.destroy');
+        });
+    });
+});
