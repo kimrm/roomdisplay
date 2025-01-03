84e6b0927ddb94fac6e4024be1b63f85c8bf52b0
Kim Rune Møller <kimrune@hotmail.no>
2025-01-03 11:56:53 +0100

    fix layout room table

--- resources/js/Pages/Admin/Rooms/Index.tsx
+++ resources/js/Pages/Admin/Rooms/Index.tsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import { Room } from "@/types";
 
 interface RoomProps {
@@ -19,14 +19,25 @@ export default function RoomsPage({ rooms }: RoomProps) {
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div className="flex justify-end">
+                        <Link
+                            href="/dashboard/rooms/create"
+                            className="rounded-md bg-white px-4 py-2 hover:bg-gray-200 dark:bg-gray-800 dark:text-white"
+                        >
+                            Nytt rom
+                        </Link>
+                    </div>
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
-                        <p className="dark:text-white">Rom</p>
-                        <table>
-                            <caption className="sr-only">Rom</caption>
+                        <h3 className="mb-4 dark:text-white">Romoversikt</h3>
+                        <table className="w-full">
+                            <caption className="sr-only">Romoversikt</caption>
                             <thead>
                                 <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
+                                    <th className="text-start">ID</th>
+                                    <th className="text-start">Name</th>
+                                    <th className="text-start">Lokasjon</th>
+                                    <th className="text-start">Synk.</th>
+                                    <th className="text-start">Link</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -34,6 +45,26 @@ export default function RoomsPage({ rooms }: RoomProps) {
                                     <tr key={room.id}>
                                         <td>{room.id}</td>
                                         <td>{room.name}</td>
+                                        <td>{room.location?.name}</td>
+                                        <td>
+                                            {room.service ? (
+                                                <span className="text-green-500">
+                                                    Aktiv
+                                                </span>
+                                            ) : (
+                                                <span className="text-yellow-500">
+                                                    Inaktiv
+                                                </span>
+                                            )}
+                                        </td>
+                                        <td>
+                                            <a
+                                                href={`/locations/${room.location?.slug}/room/${room.slug}`}
+                                                target="blank"
+                                            >
+                                                {room.slug}
+                                            </a>
+                                        </td>
                                     </tr>
                                 ))}
                             </tbody>
