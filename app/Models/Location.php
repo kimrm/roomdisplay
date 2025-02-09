e041d34f37c7f905f21d25d426bd77de673f08cd
Kim Rune Møller <kimrune@hotmail.no>
2025-02-10 00:10:07 +0100

    add fillable and generate slug method

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,10 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Support\Str;
 
 class Location extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'display_message',
+        'slug'
+    ];
 
     public function rooms(): HasMany
     {
@@ -34,5 +42,19 @@ class Location extends Model
     public function bookings(): HasManyThrough
     {
         return $this->hasManyThrough(Booking::class, Room::class);
+    }
+
+    public static function generateUniqueSlug($name)
+    {
+        $slug = Str::slug($name);
+        $originalSlug = $slug;
+        $count = 1;
+
+        while (self::where('slug', $slug)->exists()) {
+            $slug = "{$originalSlug}-{$count}";
+            $count++;
+        }
+
+        return $slug;
     }
 }
