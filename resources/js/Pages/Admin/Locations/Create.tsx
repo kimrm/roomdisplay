32d36002dddb7ffb10bf435b554ffa82520cdc05
Kim Rune Møller <kimrune@hotmail.no>
2025-02-10 00:10:41 +0100

    locations views

---
+++ resources/js/Pages/Admin/Locations/Create.tsx
@@ -0,0 +1,109 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import PrimaryButton from "@/Components/PrimaryButton";
+import SecondaryButton from "@/Components/SecondaryButton";
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
+import { router, useForm } from "@inertiajs/react";
+
+export default function RoomsCreate() {
+    const { data, setData, post, processing, errors } = useForm({
+        name: "",
+        description: "",
+        display_message: "",
+    });
+
+    function handleChange(
+        event: React.ChangeEvent<
+            HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+        >,
+    ) {
+        setData(event.target.name as keyof typeof data, event.target.value);
+    }
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        post("/dashboard/locations");
+    }
+
+    function handleCancel() {
+        router.visit("/dashboard/locations");
+    }
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    Lokasjoner
+                </h2>
+            }
+        >
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
+                        <h3 className="mb-4 dark:text-white">
+                            Opprett lokasjon
+                        </h3>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="name">Navn</InputLabel>
+                                <TextInput
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    value={data.name}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm md:w-1/2"
+                                />
+                                {errors.name && (
+                                    <em className="block text-red-500">
+                                        {errors.name}
+                                    </em>
+                                )}
+                            </div>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="description">
+                                    Beskrivelse
+                                </InputLabel>
+                                <TextInput
+                                    id="description"
+                                    name="description"
+                                    type="text"
+                                    value={data.description}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm md:w-1/2"
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="display_message">
+                                    Visningsmelding
+                                </InputLabel>
+                                <textarea
+                                    id="display_message"
+                                    name="display_message"
+                                    value={data.display_message}
+                                    onChange={handleChange}
+                                    rows={10}
+                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                />
+                            </div>
+
+                            <div className="mt-6 space-x-4">
+                                <PrimaryButton
+                                    type="submit"
+                                    disabled={processing}
+                                >
+                                    Lagre
+                                </PrimaryButton>
+                                <SecondaryButton
+                                    type="button"
+                                    onClick={handleCancel}
+                                    disabled={processing}
+                                >
+                                    Avbryt
+                                </SecondaryButton>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
---
+++ resources/js/Pages/Admin/Locations/Index.tsx
@@ -0,0 +1,81 @@
+import { PageProps } from "@/types";
+import { Location } from "@/types";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { Head, Link } from "@inertiajs/react";
+
+export default function Index({
+    locations,
+}: PageProps<{ locations: Location[] }>) {
+    return (
+        <Authenticated
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    Lokasjoner
+                </h2>
+            }
+        >
+            <Head title="Lokasjoner" />
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
+                        <div className="mb-4 flex items-center justify-between">
+                            <h3 className="text-xl dark:text-white">
+                                Lokasjoner
+                            </h3>
+                            <Link
+                                href="/dashboard/locations/create"
+                                className="flex items-center justify-center rounded-md border border-transparent px-4 py-2 hover:border-neutral-200 dark:bg-gray-800 dark:text-white"
+                            >
+                                <svg
+                                    fill="none"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                    className="mr-2 inline-block h-6 w-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                                    />
+                                </svg>
+                                Ny lokasjon
+                            </Link>
+                        </div>
+                        <ul>
+                            {locations.map((location, i) => (
+                                <li
+                                    key={location.id}
+                                    className={`${i % 2 === 0 ? "bg-neutral-100" : "bg-neutral-200"} mb-2 space-y-2 rounded-md p-4`}
+                                >
+                                    <div className="flex items-center justify-between">
+                                        <div>
+                                            <a
+                                                href={`/${location.slug}`}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                                className="text-blue-500 hover:text-blue-600"
+                                            >
+                                                {location.name}
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <Link
+                                                href={`/dashboard/locations/${location.id}/edit`}
+                                                className="text-blue-500 hover:text-blue-600"
+                                            >
+                                                Rediger
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </Authenticated>
+    );
+}
