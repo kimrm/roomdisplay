d80dce331989b81b9dd2f68b7fb939e8b25a1dde
Kim Rune Møller <kimrune@hotmail.no>
2025-02-27 11:32:05 +0100

    refactor

--- resources/js/Pages/Admin/Customers/Show.tsx
+++ resources/js/Pages/Admin/Customers/Show.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 import { PageProps } from "@/types";
@@ -12,14 +11,17 @@ export default function Show({
         <Authenticated>
             <Head title="Kunder" />
             <div className="py-12">
-                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8 dark:text-white">
+                <div className="mx-auto max-w-7xl space-y-6 text-gray-900 sm:px-6 lg:px-8 dark:text-white">
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
                         <div className="mb-4 flex items-center justify-between">
                             <h3 className="text-xl dark:text-white">
+                                <span className="text-gray-400">
+                                    #{customer.id}
+                                </span>{" "}
                                 {customer.name}
                             </h3>
                             <Link
-                                href="/dashboard/locations/create"
+                                href={route("customers.edit", customer.id)}
                                 className="flex items-center justify-center rounded-md border border-transparent px-4 py-2 hover:border-neutral-200 dark:bg-gray-800 dark:text-white"
                             >
                                 <svg
@@ -34,70 +36,61 @@ export default function Show({
                                     <path
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
-                                        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
                                     />
                                 </svg>
-                                Rediger
+                                <span className="hidden md:block">Rediger</span>
                             </Link>
                         </div>
                         <div>
                             <div className="mb-2 space-y-2 rounded-md bg-neutral-100 p-4 dark:bg-gray-900">
-                                <h3 className="text-xl dark:text-white">
-                                    Kundeinfo
-                                </h3>
-                                <div>
-                                    <p>
-                                        <span className="text-sm uppercase text-gray-400">
-                                            e-post:{" "}
-                                        </span>
-                                        {customer.email}
-                                    </p>
-                                    <p>
-                                        {" "}
-                                        <span className="text-sm uppercase text-gray-400">
-                                            tlf:{" "}
-                                        </span>
-                                        {customer.phone}
-                                    </p>
-                                    <p>
-                                        {" "}
-                                        <span className="text-sm uppercase text-gray-400">
-                                            adresse:{" "}
-                                        </span>
-                                        {customer.address}
-                                    </p>
-                                    <p>
-                                        {" "}
-                                        <span className="text-sm uppercase text-gray-400">
-                                            sted:{" "}
-                                        </span>
-                                        {customer.city}
-                                    </p>
-                                    <p>
-                                        {" "}
-                                        <span className="text-sm uppercase text-gray-400">
-                                            postnr:{" "}
-                                        </span>
-                                        {customer.postalCode}
-                                    </p>
-                                    <p>
-                                        {" "}
-                                        <span className="text-sm uppercase text-gray-400">
-                                            kontakt:{" "}
-                                        </span>
-                                        {customer.contactName}
-                                    </p>
-                                    <p>
-                                        {" "}
-                                        <span className="text-sm uppercase text-gray-400">
-                                            org.nr:{" "}
-                                        </span>
-                                        {customer.orgNr}
-                                    </p>
+                                <div className="space-y-2">
+                                    <div className="flex items-center">
+                                        <div className="inline-block w-full max-w-32 text-xs uppercase text-gray-400">
+                                            e-post{" "}
+                                        </div>
+                                        <div>{customer.email}</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="inline-block w-full max-w-32 text-xs uppercase text-gray-400">
+                                            telefon{" "}
+                                        </div>
+                                        <div>{customer.phone}</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="inline-block w-full max-w-32 text-xs uppercase text-gray-400">
+                                            adresse{" "}
+                                        </div>
+                                        <div>{customer.address}</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="inline-block w-full max-w-32 text-xs uppercase text-gray-400">
+                                            sted{" "}
+                                        </div>
+                                        <div>{customer.city}</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="inline-block w-full max-w-32 text-xs uppercase text-gray-400">
+                                            postnummer{" "}
+                                        </div>
+                                        <div>{customer.postalCode}</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="inline-block w-full max-w-32 text-xs uppercase text-gray-400">
+                                            kontaktperson{" "}
+                                        </div>
+                                        <div>{customer.contactName}</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="inline-block w-full max-w-32 text-xs uppercase text-gray-400">
+                                            org.nummer{" "}
+                                        </div>
+                                        <div>{customer.orgNr}</div>
+                                    </div>
                                 </div>
                             </div>
                             <div className="mb-2 space-y-2 rounded-md bg-neutral-100 p-4 dark:bg-gray-900">
-                                <h3 className="text-xl dark:text-white">
+                                <h3 className="text-xs uppercase text-gray-400">
                                     Notater
                                 </h3>
                                 <div>
@@ -106,7 +99,7 @@ export default function Show({
                             </div>
 
                             <div className="mb-2 space-y-2 rounded-md bg-neutral-100 p-4 dark:bg-gray-900">
-                                <h3 className="text-xl dark:text-white">
+                                <h3 className="text-xs uppercase text-gray-400">
                                     Bookinger
                                 </h3>
                                 <ul>
